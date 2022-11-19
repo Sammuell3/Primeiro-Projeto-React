94763685b810a0b9f952e3166488d59b75281210
Samuel Barros <115745745+Sammuell3@users.noreply.github.com>
2022-11-19 00:04:50 -0300

    first commit

---
+++ src/Header.tsx
@@ -0,0 +1,14 @@
+import "./header.css"
+function Header() {
+
+    return (
+          //   <img src="https://imgs.search.brave.com/TpmilWCMn6_k1aXOG5quxI1eh4JPkeX0dsTsOJ0IcsI/rs:fit:1200:1080:1/g:ce/aHR0cHM6Ly93d3cu/bWV1d2luZG93cy5j/b20vd3AtY29udGVu/dC91cGxvYWRzLzIw/MjAvMTIvZ2l0aHVi/LWxvZ28tYmFja2dy/b3VuZC5qcGc" alt="foto-git" />
+        <div className="conteiner">
+            
+           <button><a href="https://github.com/Sammuell3" target={"_blank"} title="Link do Repositorio github">Sammuell3</a></button>
+            
+        </div>
+
+    )
+}
+export default Header
